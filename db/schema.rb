1d040fbbac3624bd56c4e51d33f7b0f8be0d3b0e
kodai-html <daietto.72@gmail.com>
2021-02-16 11:51:59 +0900

    deviseのマイグレーションの実行

--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,14 @@
 ActiveRecord::Schema.define(version: 2021_02_16_005220) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "last_name", null: false
+    t.string "first_name", null: false
+    t.string "kana_last", null: false
+    t.string "kana_first", null: false
+    t.date "birth_day", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
