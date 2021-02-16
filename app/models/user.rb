54ae711565050bd310abdd060bd1ddece6e1fa74
kodai-html <daietto.72@gmail.com>
2021-02-17 00:11:43 +0900

    バリデーションの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :email, presence: true,　uniquness: true, inclusion: { in: @ }
+  validates :password, length: {minimum: 6}, format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/}
+  validates :password, password_confirmation: true
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :kana_last, presence: true, format: { with: /^[ァ-ンヴー]+$/}
+  validates :kana_first, presence: true, format: { with: /^[ァ-ンヴー]+$/}
+  validates :birth_day, presence: true
 end
