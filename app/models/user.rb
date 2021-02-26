4d72380549d7488f61af8770bb7a67356c981515
kodai-html <daietto.72@gmail.com>
2021-02-26 14:34:29 +0900

    テストコードの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :email, presence: true,　uniquness: true, inclusion: { in: @ }
   validates :password, length: {minimum: 6}, format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/}
-  validates :password, password_confirmation: true
   validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
-  validates :kana_last, presence: true, format: { with: /^[ァ-ンヴー]+$/}
-  validates :kana_first, presence: true, format: { with: /^[ァ-ンヴー]+$/}
+  validates :kana_last, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
+  validates :kana_first, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
   validates :birth_day, presence: true
 end
