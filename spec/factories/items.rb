30a47660835675a50e5db2f8a985a22a44af5183
kodai-html <daietto.72@gmail.com>
2021-03-01 11:35:57 +0900

    修正２

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,11 +2,11 @@ FactoryBot.define do
   factory :item do
     name              {'名前'}
     explain           {'説明'}
-    category_id       {'2'}
-    state_id          {'2'}
-    burden_id         {'2'} 
-    prefecture_id     {'2'}
-    delivery_id       {'2'}
+    category_id       {2}
+    state_id          {2}
+    burden_id         {2} 
+    prefecture_id     {2}
+    delivery_id       {2}
     price             {300}
     association :user
 
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -23,29 +23,29 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Explain can't be blank")
       end
       it 'カテゴリーが選択されてなければ登録できない' do
-        @item.category_id = "--"
+        @item.category_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Category is not a number")
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
       end
       it '商品の状態が選択されていなければ登録できない' do
-        @item.state_id = "--"
+        @item.state_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("State is not a number")
+        expect(@item.errors.full_messages).to include("State must be other than 1")
       end
       it '配送料の負担が選択されいなければ登録できない' do
-        @item.burden_id = "--"
+        @item.burden_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Burden is not a number")
+        expect(@item.errors.full_messages).to include("Burden must be other than 1")
       end
       it '発送元が選択されていなければ登録できない' do
-        @item.prefecture_id = "--"
+        @item.prefecture_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Prefecture is not a number")
+        expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
       end
       it '日数が選択されていなければ登録できない' do
-        @item.delivery_id = "--"
+        @item.delivery_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Delivery is not a number")
+        expect(@item.errors.full_messages).to include("Delivery must be other than 1")
       end
       it '価格がなければ登録できない' do
         @item.price = ""
@@ -57,11 +57,20 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Price ¥300~¥9.999,999でお願いします")
       end
+      it '価格は¥300~¥9.999,999でなければ登録できない' do
+        @item.price = 100000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price ¥300~¥9.999,999でお願いします")
+      end
       it '販売価格は半角数字でなければ登録できない' do
         @item.price = "１０００"
         @item.valid?
         expect(@item.errors.full_messages).to include("Price 半角数字でお願いします。")
-
+      end
+      it '画像がなければ登録できない。' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
       end
     end
   end
