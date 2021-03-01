3653a6b2e19fc747bf8e442a1f3e662951fc10c0
kodai-html <daietto.72@gmail.com>
2021-03-01 12:01:26 +0900

    修正３

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,7 +12,7 @@ class Item < ApplicationRecord
 
   
 
-  validates :image, :name, :explain, :category_id, :state_id, :burden_id, :prefecture_id, :delivery_id, :price, :user, presence: true
+  validates :image, :name, :explain, :category_id, :state_id, :burden_id, :prefecture_id, :delivery_id, :price, presence: true
   validates :category_id, :state_id, :burden_id, :prefecture_id, :delivery_id, numericality: { other_than: 1 }
   validates :price, numericality: { only_integer: true, message: '半角数字でお願いします。'}
   validates :price, inclusion: {in: 300..9999999, message: '¥300~¥9.999,999でお願いします'}
