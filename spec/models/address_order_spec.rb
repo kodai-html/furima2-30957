e3ba1107a495b7c0d2d7ceef745dc44fe526a0a1
kodai-html <daietto.72@gmail.com>
2021-03-11 11:22:12 +0900

    テストコードの編集

--- spec/models/address_order_spec.rb
+++ spec/models/address_order_spec.rb
@@ -56,9 +56,9 @@ RSpec.describe AddressOrder, type: :model do
         expect(@address.errors.full_messages).to include("Number can't be blank")
       end
       it "tokenが空では登録できないこと" do
-        @order.token = nil
-        @order.valid?
-        expect(@order.errors.full_messages).to include("Token can't be blank")
+        @address.token = nil
+        @address.valid?
+        expect(@address.errors.full_messages).to include("Token can't be blank")
       end
     end
   end
